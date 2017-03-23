3845dc3add1c5656477f8ae3d0da222aeedf05a5
khrsman <khrsman@gmail.com>
2017-03-23 07:58:23 +0700

    add existing

---
+++ application/modules/editor/controllers/Editor.php
@@ -0,0 +1,22 @@
+<?php
+
+
+ if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Editor extends CI_Controller {
+
+    function __construct()
+    {
+        parent::__construct();
+        //$this->load->model('m_chat');
+
+    }
+
+    function index(){
+		echo 'kaharisman';
+		//$this->load->view('index.php');
+    }
+      
+
+}
+?>
